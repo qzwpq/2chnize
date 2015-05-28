e4feb1e1d12ceb12ed51142234ef94c44b5e5a84
qzwpq <qzwpqpwq@gmail.com>
2015-05-29 03:37:28 +0900

    Fourth commit

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 'use strict';
 
+var Vue = require('vue');
+
 var Twit = require('twit');
 
-var KEYS = require('./config.json');
+var fs = require('fs');
+
+var KEYS = require('./keys.json');
+
+var config = require('./config.json');
 
 var T = new Twit(KEYS.product);
 
 var stream = T.stream('user');
-
+// note that the last function will be executed at first
 var rules = [{
-	condition: function(tweet) {
-		return !!tweet.retweeted_status;
+	condition: function() {
+		return true;
 	},
 	func: function(tweet) {
-		tweet.text = tweet.retweeted_status.text;
+		var rule = JSON.parse(fs.readFileSync('./replaceRules.json', {
+			encoding: 'utf-8'
+		}));
+		for (var i = rule.length - 1; i >= 0; i--) {
+			var reg = new RegExp(rule[i].reg, rule[i].flag);
+			tweet.text = tweet.text.replace(reg, rule[i].newSubStr);
+		}
+		return tweet;
+	}
+}, {
+	condition: function() {
+		return true;
+	},
+	func: function(tweet) {
+		tweet.text = tweet.text.replace(/\n/g, '<br>');
 		return tweet;
 	}
 }, {
@@ -22,18 +42,24 @@ var rules = [{
 	},
 	func: function(tweet) {
 		for (var i = tweet.entities.urls.length - 1; i >= 0; i--) {
-			tweet.text = tweet.text.replace(tweet.entities.urls[i].url, tweet.entities.urls[i].expanded_url);
+			tweet.text = tweet.text.replace(tweet.entities.urls[i].url, '<a href="tweet.entities.urls[i].expanded_url">' + tweet.entities.urls[i].display_url + '</a>');
 		}
 		return tweet;
 	}
 }, {
 	condition: function(tweet) {
-		return !!tweet.entities.media;
+		return typeof tweet.entities.media !== 'undefined';
 	},
 	func: function(tweet) {
+		tweet.text += '<br>';
 		for (var i = tweet.extended_entities.media.length - 1; i >= 0; i--) {
-			tweet.text = tweet.text.replace(tweet.extended_entities.media[i].url, '');
-			tweet.text += ' ' + tweet.extended_entities.media[i].media_url
+			var media = tweet.extended_entities.media[i];
+			tweet.text = tweet.text.replace(media.url, '');
+			if (media.type === 'animated_gif') {
+				tweet.text += '<video height="128" loop><source src="' + media.video_info.variants[0].url + '" type="' + media.video_info.variants[0].content_type + '"></video>';
+			} else {
+				tweet.text += '<img src="' + media.media_url + '" height="128" >';
+			}
 		}
 		return tweet;
 	}
@@ -42,13 +68,11 @@ var rules = [{
 		return true;
 	},
 	func: function(tweet, tweetStorage) {
-		var flag = true;
 		for (var i = tweet.entities.user_mentions.length - 1; i >= 0; i--) {
-			if ((!!tweet.in_reply_to_status_id_str) && flag) {
+			if (tweet.id_str === tweet.in_reply_to_status_id_str) {
 				for (var j = tweetStorage.length - 1; j >= 0; j--) {
 					if (tweetStorage[j].id_str === tweet.in_reply_to_status_id_str) {
 						tweet.text = tweet.text.replace('@' + tweet.entities.user_mentions[i].screen_name, '&gt;&gt;' + tweetStorage[j].count)
-						flag = false;
 						break;
 					}
 				}
@@ -56,17 +80,44 @@ var rules = [{
 			} else {
 				tweet.text = tweet.text.replace('@' + tweet.entities.user_mentions[i].screen_name, '');
 			}
-		};
+		}
+		return tweet;
+	}
+}, {
+	condition: function(tweet) {
+		return typeof tweet.retweeted_status !== 'undefined';
+	},
+	func: function(tweet) {
+		tweet = tweet.retweeted_status;
 		return tweet;
 	}
 }];
 
 var ngRules = [
 	function(tweet) {
-		var rule = require('./ngRules.json');
+		var rule = JSON.parse(fs.readFileSync('./ngRules.json', {
+			encoding: 'utf-8'
+		}));
 		for (var i = rule.length - 1; i >= 0; i--) {
 			var reg = new RegExp(rule[i].reg, rule[i].flag);
 			var result = reg.test(tweet.text);
+			if (result) {
+				return true;
+			}
+		}
+		return false;
+	},
+	function(tweet) {
+		var rule = JSON.parse(fs.readFileSync('./ngClients.json', {
+			encoding: 'utf-8'
+		}));
+		var clientName = (function(tweet) {
+			var d = document.createElement('div').innerHTML = tweet.source;
+			return d.textContent;
+		})(tweet);
+		for (var i = rule.length - 1; i >= 0; i--) {
+			var reg = new RegExp(rule[i].reg, rule[i].flag);
+			var result = reg.test(clientName);
 			if (result) {
 				return true;
 			}
@@ -78,7 +129,7 @@ var ngRules = [
 var tweets = (function() {
 	var tweetStorage = [];
 	var count = 0;
-	return (function(tweet) {
+	return function(tweet) {
 		var hash = require('crypto').createHash('sha1');
 		tweet.count = ++count;
 		tweetStorage.push(tweet);
@@ -92,13 +143,23 @@ var tweets = (function() {
 				return;
 			}
 		}
-		hash.update(tweet.user.screen_name + (new Date()).toDateString());
-		var isBottom = (window.scrollY === (document.body.scrollHeight - document.body.clientHeight));
-		document.write(count + ': Anonymous ' + Date() + ' ID: ' + hash.digest('base64').slice(0, 9) + '<br>' + tweet.text + '<br>');
+		var date = new Date(tweet.created_at);
+		hash.update(tweet.user.screen_name + date.toDateString());
+		// document.documentElement.scrollHeight - document.documentElement.clientHeight === document.scrollMaxY
+		var isBottom = (window.scrollY >= (document.documentElement.scrollHeight - document.documentElement.clientHeight));
+		var dateString = (function(d) {
+			var pad = function(s) {
+				s = s + '';
+				return s.length === 1 ? '0' + s : s;
+			};
+			return [d.getFullYear(), d.getMonth(), d.getDate()].map(pad).join('/') +
+				' ' + [d.getHours(), d.getMinutes(), d.getSeconds()].map(pad).join(':');
+		})(date);
+		document.getElementById('main').innerHTML += (count + ': ' + config.nanashi + ' ' + dateString + ' ID:' + hash.digest('base64').slice(0, 9) + '<br>' + tweet.text + '<br>');
 		if (isBottom) {
 			document.body.scrollTop = document.body.scrollHeight;
 		}
-	});
+	};
 })();
 
 stream.on('tweet', function(t) {
