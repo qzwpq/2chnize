1ad13355f4642f1c39e0a02f1ec187b86ec75649
qzwpq <qzwpqpwq@gmail.com>
2015-09-13 00:54:34 +0900

    avoid double escaping

--- src/util/prepareTweet.js
+++ src/util/prepareTweet.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
 import getTextParts from './getTextParts';
 import getMediaParts from './getMediaParts';
 import createHashedId from './createHashedId';
 import makeDateString from './makeDateString';
 
 export default (tweet, timeline) => {
+	tweet.text = _.unescape(tweet.text); // avoid double escaping
 	tweet.textParts = getTextParts(tweet);
 	tweet.mediaParts = getMediaParts(tweet);
 	tweet.hashedId = createHashedId(tweet.user.screen_name, tweet.created_at);
